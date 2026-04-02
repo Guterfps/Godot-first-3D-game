bf350016dd4e8cae48992075883110b770b6d196
Guterfps <ithayg@gmail.com>
2026-04-02 12:26:30 +0300

    player movment

---
+++ scenes/player/player.gd
@@ -0,0 +1,36 @@
+extends CharacterBody3D
+
+@export var speed: float = 14
+@export var fall_acceleration: float = 75
+
+var target_velocity = Vector3.ZERO
+
+func _physics_process(delta: float) -> void:
+	var direction = Vector3.ZERO
+	
+	if Input.is_action_pressed("move_tight"):
+		direction.x += 1
+	if Input.is_action_pressed("move_left"):
+		direction.x -= 1
+	if Input.is_action_pressed("move_back"):
+		direction.z += 1
+	if Input.is_action_pressed("move_forward"):
+		direction.z -= 1
+	
+	if direction != Vector3.ZERO:
+		direction = direction.normalized()
+		$Pivot.basis = Basis.looking_at(direction)
+	
+	ground_velocity(direction)
+	vertical_velocity(delta)
+	
+	velocity = target_velocity
+	move_and_slide()	
+
+func ground_velocity(direction: Vector3):
+		target_velocity.x = direction.x * speed
+		target_velocity.z = direction.z * speed
+
+func vertical_velocity(delta: float):
+	if not is_on_floor():
+		target_velocity.y = target_velocity.y - (fall_acceleration * delta)
